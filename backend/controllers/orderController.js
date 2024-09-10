539a61ce54c72cb81fc9f67b498f52051e8adf32
Aditya <axdityax25@gmail.com>
2024-09-10 22:01:25 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-	const frontend_url = "http://localhost:5174";
+	const frontend_url = "https://feastify-frontend-ip1a.onrender.com/";
 
 	try {
 		const newOrder = new orderModel({
